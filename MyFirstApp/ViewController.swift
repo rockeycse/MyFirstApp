bf59611a0dddeb5ff3becc5310a96a7560b0f649
Rocky Ahmed <rockyahmed@Rockys-Mac-mini.local>
2017-09-22 21:15:06 +0600

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Rocky Ahmed  on 9/22/17.
+//  Copyright © 2017 Rocky Ahmed . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
